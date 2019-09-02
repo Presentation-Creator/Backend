3d409c9f605334616850b01318a7f7e19d5cae25
Max Base <MaxBaseCode@Gmail.Com>
2019-09-02 23:06:05 +0430

    Update UserForRegisterDto.cs

--- Dtos/UserForRegisterDto.cs
+++ Dtos/UserForRegisterDto.cs
@@ -5,13 +5,13 @@ namespace PresentationCreatorWeb.Dtos
 {
     public class UserForRegisterDto
     {
-        [Required(ErrorMessage="فیلد ایمیل اجباری است")]
-        [EmailAddress(ErrorMessage="فیلد ایمیل صحیح نیست")]
+        [Required(ErrorMessage="Email field is required.")]
+        [EmailAddress(ErrorMessage="Email field is not correct.")]
         public string Email { get; set; }
-        [Required(ErrorMessage="فیلد شماره تلفن اجباری است")]
+        [Required(ErrorMessage="Phone number field required.")]
         public string Phone { get; set; }
-        [Required(ErrorMessage="فیلد پسورد اجباری است")]
-        [StringLength(8, MinimumLength = 4, ErrorMessage = "پسورد شما باید بین 4 تا 8 کاراکتر داشته باشد")]
+        [Required(ErrorMessage="Password field is required.")]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "Password length must be between 4 and 8 characters long.")]
         public string Password { get; set; }
     }
 }
